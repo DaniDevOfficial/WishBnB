db82cddbbb6a54dde1e40d26f22d9ab5ce44f4de
David <bischof.david.db@gmail.com>
2023-12-23 23:00:59 +0100

    added stats

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Landing from '../components/Home/Landing'
+import { Stats } from '../components/Home/Stats'
 
 export function HomePage() {
 
     return (
         <>
             <Landing/>
+            <Stats />
         </>
     )
 }
