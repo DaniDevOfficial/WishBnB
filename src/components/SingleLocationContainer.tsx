4352b9a344aebc85cb4be7f0e4f5444d63191c3d
DavDevDav <bischof.david.db@gmail.com>
2023-12-24 15:21:07 +0100

    fixed everything

--- src/components/SingleLocationContainer.tsx
+++ src/components/SingleLocationContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Image, Heading, Text, Skeleton, useColorMode, Button } from '@chakra-ui/react';
+import { Box, Image, Heading, Text, Skeleton, useColorMode, Button, useColorModeValue } from '@chakra-ui/react';
 
 export function SingleLocationContainer({ post }: { post: any }) {
     const { title, image, description, price } = post;
@@ -15,7 +15,7 @@ export function SingleLocationContainer({ post }: { post: any }) {
     }, []);
 
     return (
-        <Box boxShadow="md" borderRadius="md" width={"1xl"} p={4}>
+        <Box boxShadow="rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;" borderRadius="md" width={"1xl"} p={4} >
             {isLoading ? (
                 <>
                     <Skeleton height="160px" width="270px" borderRadius="md" mb="2" />
@@ -38,22 +38,23 @@ export function SingleLocationContainer({ post }: { post: any }) {
                     loading="lazy"
                 />
             )}
-            <Text fontSize={"xs"} color={colorMode === 'dark' ? "smalltext.base" : "smalltext.base"} mb="1">
+            <Text fontSize={"xs"} color={colorMode === 'dark' ? "smalltext.base" : "smalltext.base"} mb="1" display={isLoading ? "none" : "block"}>
                 {`Location: ${title}`}
             </Text>
-            <Heading as="h3" size="md" mb="1" color={colorMode === 'dark' ? "text.darkmode" : "black"}>
+            <Heading as="h3" size="md" mb="1" color={colorMode === 'dark' ? "text.darkmode" : "black"} display={isLoading ? "none" : "block"}>
                 {title}
             </Heading>
-            <Text fontSize="sm" color={colorMode === 'dark' ? "text.darkmode" : "text.base"} mb="2" maxW="270px">
+            <Text fontSize="sm" color={colorMode === 'dark' ? "text.darkmode" : "text.base"} mb="2" maxW="270px" display={isLoading ? "none" : "block"}>
                 {description}
             </Text>
-            <Text fontWeight="bold" fontSize="lg" color={colorMode === 'dark' ? "secondary.darkmode" : "secondary.base"}>
+            <Text fontWeight="bold" fontSize="lg" color={colorMode === 'dark' ? "secondary.darkmode" : "secondary.base"} display={isLoading ? "none" : "block"}>
                 {`Price Per Night: ${price}.00 CHF`}
             </Text>
             <Button
                 mt={4}
                 color={colorMode === 'dark' ? 'primary.darkmode' : 'primary.base'}
-                 size="sm"
+                size="sm"
+                display={isLoading ? "none" : "block"}
                 onClick={() => {
                     alert("You have booked this location!");
                 }}
